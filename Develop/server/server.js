6ea6b299c3f7ad52fd6f2a234036cb5f2278f30f
Chris San <christianes322@gmail.com>
2023-12-08 00:16:33 -0600

    i fucking give up

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
-const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./Schemas');
 const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -23,8 +24,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
